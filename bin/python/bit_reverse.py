7f20ada2f2095f500d9c0362d5c117fdd21aea96
Guoyou Jiang <guoyou.jiang@spreadtrum.com>
2018-07-03 15:00:50 +0800

    more efficient coding

--- bin/python/bit_reverse.py
+++ bin/python/bit_reverse.py
@@ -3,13 +3,11 @@
 infile = open('efuse.vhx32','r',-1)
 otfile = open('efuse.bit_reverse.vhx32','w',-1)
 
-all_lines = infile.readlines()
-
-for i in range(0,len(all_lines)):
-    value = all_lines[i][:-1]   # remove \n
+for line in infile:
+    value = line[:-1]      # remove '\n'
     value = int(value,16)
     b     = '{0:#0{1}x}'.format( int('{:032b}'.format(value)[::-1],2),10 )+'\n'
-    otfile.write(b[2:])
+    otfile.write(b[2:])    # remove leading '0x'
 
 infile.close()
 otfile.close()
