b35c04b7c3c5145e70693e529b027c5c2913e178
Guoyou Jiang <guoyou.jiang@spreadtrum.com>
2018-07-03 13:55:38 +0800

    update to file manipulation

--- bin/python/bit_reverse.py
+++ bin/python/bit_reverse.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-f=open('bit_reverse.vhx32','w')
+infile = open('efuse.vhx32','r',-1)
+otfile = open('efuse.bit_reverse.vhx32','w',-1)
 
-f.write( '{0:#0{1}x}'.format( int('{:032b}'.format(0xfffffff2)[::-1],2),10 )+'\n' )
-f.write( '{0:#0{1}x}'.format( int('{:032b}'.format(0x0000000a)[::-1],2),10 )+'\n' )
-f.write( '{0:#0{1}x}'.format( int('{:032b}'.format(0x9030f50c)[::-1],2),10 )+'\n' )
-f.write( '{0:#0{1}x}'.format( int('{:032b}'.format(0x00010005)[::-1],2),10 )+'\n' )
+all_lines = infile.readlines()
 
-f.close()
+for i in range(0,len(all_lines)):
+    value = all_lines[i][:-1]   # remove \n
+    value = int(value,16)
+    b     = '{0:#0{1}x}'.format( int('{:032b}'.format(value)[::-1],2),10 )+'\n'
+    otfile.write(b[2:])
+
+infile.close()
+otfile.close()
 
